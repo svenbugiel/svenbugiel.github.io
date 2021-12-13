d337e7300ac4e2118e8220daf5c8743986567b51
Sven Bugiel <bugiel@cs.uni-saarland.de>
2021-12-13 09:58:49 +0100

    updated affiliation to tenured faculty

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/var/folders/xb/3yy2cv815xg41v4dcykxkp3w0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210516101231-84e462a5a149/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
